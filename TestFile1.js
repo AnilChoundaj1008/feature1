b666033710d5d10e730237f0b41d1d43572de41f
AnilChoundaj1008 <choundajanil@gmail.com>
2024-07-26 12:04:17 +1000

    merge check

--- TestFile1.js
+++ TestFile1.js
@@ -1,4 +1,4 @@
 console.log('hello changes from main to feature change check conflict')
 console.log('first commit from f3')
 console.log('hello 2');
-console.log('feature 2 frist comment')
+console.log('feature 2 frist comment...')
